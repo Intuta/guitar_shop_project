2dac4c629d06012d81608a49a1750aba5c44c176
Intuta <andreeva.al.ir@gmail.com>
2022-12-14 17:58:38 +0100

    Updated integration test for UserService

--- src/test/java/com/myproject/guitar_shop/integration/UserServiceTests.java
+++ src/test/java/com/myproject/guitar_shop/integration/UserServiceTests.java
@@ -3,30 +3,112 @@ package com.myproject.guitar_shop.integration;
 import com.myproject.guitar_shop.GuitarShopApplication;
 import com.myproject.guitar_shop.domain.User;
 import com.myproject.guitar_shop.service.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = GuitarShopApplication.class)
 @SpringBootTest
+@ContextConfiguration(classes = GuitarShopApplication.class)
 @Sql(scripts = "/test-schema.sql")
 public class UserServiceTests {
     @Autowired
     private UserService userService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    private User user;
+    private Map<String, String> attributes;
+
+    @Before
+    public void initialization() {
+        user = User.builder()
+                .name("ann")
+                .email("ann@email.com")
+                .phone("12314")
+                .password(passwordEncoder.encode("password"))
+                .role(User.Role.CUSTOMER)
+                .build();
+
+        attributes = new HashMap<>();
+        attributes.put("name", "name");
+        attributes.put("email", "email");
+        attributes.put("phone", "phone");
+        attributes.put("role", "CUSTOMER");
+        attributes.put("password", "password");
+        attributes.put("new_password", "new_password");
+    }
 
     @Test
-    public void setItemServiceTest() {
-        userService.save(User.builder()
-                .name("Ann")
-                .email("Ann@email.com")
-                .phone("12314")
-                .password("password")
-                .role(User.Role.CUSTOMER)
-                .build());
+    public void saveTest() {
+        assertThat(userService.save(user)).isEqualTo(user).usingRecursiveComparison();
     }
+
+    @Test
+    public void getUserByEmailTest() {
+        userService.save(user);
+        User returnedUser = userService.getUserByEmail(user.getEmail());
+        assertThat(returnedUser).isEqualTo(user).usingRecursiveComparison();
+    }
+
+    @Test
+    public void getAllUsersByAttributeTest() {
+        userService.save(user);
+        List<User> expectedUserList = new ArrayList<>(Collections.singletonList(user));
+
+        List<User> returnedUserList = userService.getAllUsersByAttribute(user.getName());
+        assertThat(returnedUserList).isEqualTo(expectedUserList);
+
+        returnedUserList = userService.getAllUsersByAttribute(user.getEmail());
+        assertThat(returnedUserList).isEqualTo(expectedUserList);
+    }
+
+    @Test
+    public void updateTest() {
+        userService.save(user);
+        User expectedUser = User.builder()
+                .name(attributes.get("name"))
+                .email(attributes.get("email"))
+                .phone(attributes.get("phone"))
+                .role(User.Role.valueOf(attributes.get("role")))
+                .password(attributes.get("new_password")).build();
+
+        User returnedUser = userService.update(attributes, user.getId());
+        expectedUser.setId(returnedUser.getId());
+
+        assertThat(passwordEncoder.matches(expectedUser.getPassword(), returnedUser.getPassword())).isEqualTo(true);
+        assertThat(returnedUser.getId()).isEqualTo(expectedUser.getId());
+        assertThat(returnedUser.getName()).isEqualTo(expectedUser.getName());
+        assertThat(returnedUser.getPhone()).isEqualTo(expectedUser.getPhone());
+        assertThat(returnedUser.getRole()).isEqualTo(expectedUser.getRole());
+    }
+
+    @Test
+    public void mapUser() {
+        User expectedUser = User.builder()
+                .name(attributes.get("name"))
+                .email(attributes.get("email"))
+                .phone(attributes.get("phone"))
+                .role(User.Role.valueOf(attributes.get("role")))
+                .password(attributes.get("password")).build();
+
+        User returnedUser = userService.mapUser(attributes);
+        expectedUser.setId(returnedUser.getId());
+
+        assertThat(passwordEncoder.matches(expectedUser.getPassword(), returnedUser.getPassword())).isEqualTo(true);
+        assertThat(returnedUser.getId()).isEqualTo(expectedUser.getId());
+        assertThat(returnedUser.getName()).isEqualTo(expectedUser.getName());
+        assertThat(returnedUser.getPhone()).isEqualTo(expectedUser.getPhone());
+        assertThat(returnedUser.getRole()).isEqualTo(expectedUser.getRole());
+    }
+
 }
